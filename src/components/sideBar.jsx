1b935960edd7a8d0e74bce52895320fb6a28724f
Clint George <clintbgeorge@gmail.com>
2025-09-26 23:09:38 +0530

    Added templateCard and StatusBar
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -5,7 +5,7 @@ const DynamicSideBar = () => {
   return (
     <div className="bg-[#F9F9F9] mt-11 min-h-screen max-h-auto w-14 text-white fixed top-0 z-0 border-[#CFCFCF] border-r-[1px]">
       <div className="flex flex-col pt-9 pl-8 space-y-9 text-base font-light font-inter">
-        <Link to="/">
+        {/* <Link to="/">
           <p className="flex text-[#585858] justify-between pr-8">icon 1</p>
         </Link>
 
@@ -19,7 +19,7 @@ const DynamicSideBar = () => {
 
         <Link to="/">
           <p className="flex text-[#585858] justify-between pr-8">icon 4</p>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
