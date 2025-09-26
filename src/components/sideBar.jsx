68b5ec9bc570d4471c18e2fe05e9a70e0c3376fc
Clint George <clintbgeorge@gmail.com>
2025-09-26 22:03:16 +0530

    Added SearchBar.jsx
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 const DynamicSideBar = () => {
   return (
-    <div className="bg-[#F9F9F9] mt-10 min-h-screen max-h-auto w-14 text-white fixed top-0 z-0 border-[#CFCFCF] border-r-[1px]">
+    <div className="bg-[#F9F9F9] mt-11 min-h-screen max-h-auto w-14 text-white fixed top-0 z-0 border-[#CFCFCF] border-r-[1px]">
       <div className="flex flex-col pt-9 pl-8 space-y-9 text-base font-light font-inter">
         <Link to="/">
           <p className="flex text-[#585858] justify-between pr-8">icon 1</p>
