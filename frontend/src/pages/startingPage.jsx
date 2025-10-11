810b69ac04c542b7342580f35aa84b09e2e1f779
Clint George <clintbgeorge@gmail.com>
2025-10-11 21:25:29 +0530

    Minor Changes
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- frontend/src/pages/startingPage.jsx
+++ frontend/src/pages/startingPage.jsx
@@ -12,20 +12,20 @@ const StartingPage = () => {
         <div className="mt-4 text-[#7D7D7D] text-3xl font-inter font-extralight tracking-wide">
           Latex Redefined
         </div>
-        <div className="mt-10 text-[#3B3B3B] text-lg font-inter font-medium leading-[20px]">
+        <div className="mt-10 text-[#3B3B3B] text-base font-inter font-medium leading-[20px]">
           Get started
         </div>
 
         <div className="mt-5 gap-5 flex flex-col w-fit">
           <Link to="/template">
-            <div className="relative text-[#AB2D2D] text-lg font-inter font-medium text-nowrap border-[#AB2D2D] pl-14 pt-[1.1vh] pb-[0.7vh] pr-10 border-[1px]">
+            <div className="relative text-[#AB2D2D] text-base font-inter font-medium text-nowrap border-[#AB2D2D] pl-14 pt-[1.1vh] pb-[0.7vh] pr-10 border-[1px]">
               <span className="absolute left-5 font-playfair top-1 text-2xl">
                 +
               </span>
               Create New Project
             </div>
           </Link>
-          <div className="relative text-[#256081] text-lg font-inter font-medium text-nowrap border-[#256081] pl-14 pt-[1.1vh] pb-[0.7vh] pr-10 border-[1px]">
+          <div className="relative text-[#256081] text-base font-inter font-medium text-nowrap border-[#256081] pl-14 pt-[1.1vh] pb-[0.7vh] pr-0 border-[1px]">
             <span className="absolute left-5 top-[1.9vh]">
               <FileOpen style={{ fill: "#256081" }} className="w-4 h-4" />
             </span>
