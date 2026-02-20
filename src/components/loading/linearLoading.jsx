df552a855b657fff042c35921e6a6f1020c324f2
Clint George <clintbgeorge@gmail.com>
2026-02-20 22:40:16 +0530

    Integrated Loading Screen
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

---
+++ src/components/loading/linearLoading.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LinearLoading = () => {
+  return (
+    <div>
+      <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-md bg-black/20">
+        <div className="bg-white rounded-xl shadow-lg w-96 p-4 relative h-40 flex flex-col items-center justify-center">
+          <div className="text-center text-black font-inter font-medium mb-3">
+            <p>Creating your Project</p>
+          </div>
+          <div className="w-72 h-1 bg-gray-200 overflow-hidden relative flex">
+            <div className="absolute left-0 top-0 h-full w-1/3 bg-black animate-slide"></div>
+          </div>
+          <div className="text-center text-gray-400 font-inter font-normal text-[12px] mt-4">
+            <p>Generating Boilerplate code from AI Model</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LinearLoading;
