07b1a55529ebd3cd4f90eb6f440f6a54c190305b
Clint George <clintbgeorge@gmail.com>
2026-03-30 20:15:04 +0530

    Fix ENOENT bug related to config.json for Production mode
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- server.js
+++ server.js
@@ -74,19 +74,16 @@ const SIDECAR_PATH = isDev
   : join(process.env.RESOURCES_PATH, "sidecar");
 
 const getServerUrl = () => {
-  const configPath = path.resolve("./config.json");
+  const configPath = path.join(SETTINGS_DIR, "config.json");
 
   const rawData = fs.readFileSync(configPath, "utf-8");
   const config = JSON.parse(rawData);
 
-  const mode = config.server.mode;
-  const serverUrl = config.server.methods[mode].backendServer;
+  const mode = config?.server?.mode;
+  const serverUrl = config?.server?.methods[mode]?.backendServer;
 
   return serverUrl;
 };
-
-// const AI_SERVICE_URL = "http://localhost:5025";
-const AI_SERVICE_URL = getServerUrl();
 
 async function getActiveAIConfig() {
   try {
@@ -150,6 +147,9 @@ const jobDir = TEMP_DIR;
     fs.mkdirSync(dir, { recursive: true });
   }
 });
+
+// const AI_SERVICE_URL = "http://localhost:5025";
+const AI_SERVICE_URL = getServerUrl();
 
 //Uncomment it when the data flow for sidecar is ready
 // function extractPreamble(texContent) {
